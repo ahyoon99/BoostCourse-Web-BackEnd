030e9874614985eeb788b0185767237141aed572
ahyoon <kimay9623@naver.com>
2021-03-01 17:45:53 +0900

    Request, Response 객체 이해하기(파라미터 읽어 들이기)

---
+++ firstweb/src/examples/ParameterServlet.java
@@ -0,0 +1,48 @@
+package examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ParameterServlet
+ */
+@WebServlet("/param")
+public class ParameterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ParameterServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head><title>form</title></head>");
+		out.println("<body>");
+
+		String name = request.getParameter("name");	// parameter 이름이 "name"인 parameter값을 변수 String name에 담는다.
+		String age = request.getParameter("age");	// parameter 이름이 "age"인 parameter값을 변수 String age에 담는다.
+		
+		out.println("name : " + name + "<br>");
+		out.println("age : " +age + "<br>");
+		
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
